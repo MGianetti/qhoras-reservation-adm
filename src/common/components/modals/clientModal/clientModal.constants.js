a8a82e97b155f3613d5d46eaf7cc8bbfd6b52f17
MGianetti <39316813+MGianetti@users.noreply.github.com>
2025-08-14 23:13:36 -0300

    feat: add front end logic to new email on client domain crud (#11)

--- src/common/components/modals/clientModal/clientModal.constants.js
+++ src/common/components/modals/clientModal/clientModal.constants.js
@@ -1,6 +1,11 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
 export const validationSchema = Yup.object({
-  name: Yup.string().required("Digite o nome do cliente."),
-  phoneNumber: Yup.string().required("Digite o telefone do cliente."),
+    name: Yup.string().required('Digite o nome do cliente.'),
+    phoneNumber: Yup.string().required('Digite o telefone do cliente.'),
+    email: Yup.string()
+        .trim()
+        .transform((v, o) => (o === '' ? null : v))
+        .nullable()
+        .email('E-mail inválido.')
 });
