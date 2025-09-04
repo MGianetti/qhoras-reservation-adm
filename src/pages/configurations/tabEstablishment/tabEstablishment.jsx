f04bc1fa8f19158d91ebbeec673eebec9ee839f6
David Robert Pessoal <david_robert97@outlook.com>
2025-09-04 12:08:44 -0300

    feat: descomentado whatsappgroup

--- src/pages/configurations/tabEstablishment/tabEstablishment.jsx
+++ src/pages/configurations/tabEstablishment/tabEstablishment.jsx
@@ -23,14 +23,14 @@ function TabEstablishment() {
         <CompanyGroup />
       </Grid>
 
-      {/* <Grid
+      <Grid
         item
         xs={12}
         sx={{ mt: 2, boxShadow: theme.shadows[0] }}
         className={styles.cardGroup}
       >
         <WhatsappGroup />
-      </Grid> */}
+      </Grid>
     </Grid>
   );
 }
